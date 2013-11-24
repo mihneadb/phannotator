b5a5916f9e87078ab658d270253314f391de918b
Iulian <julian.stana@gmail.com>
2013-11-24 20:10:57 +0200

    Adding the Comment model and some fields to Annotate model.

--- server/models.py
+++ server/models.py
@@ -19,7 +19,19 @@ class Image(models.Model):
 
 
 class Annotate(models.Model):
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
     image = models.ForeignKey(Image)
+    # Florin a notat un tag cu startx/starty si width/height
+    # nu le-a salvat global, isi schimba valorile in mouseDrag
+    startX = models.IntegerField()
+    startY = models.IntegerField()
+    width  = models.IntegerField()
+    height = models.IntegerField()
 
+
+class Comment(models.Model):
+    text = models.CharField(max_length=500)
+    # let's be just a name for the start
+    author = models.CharField(max_length=50)
+    annotate = models.ForeignKey(Annotate)
 
