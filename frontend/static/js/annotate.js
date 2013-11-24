5752f4bcdfc11f5f0d84b23cb8085488c01c5abc
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-11-24 21:39:25 +0200

    use canvas for rects

---
+++ frontend/static/js/annotate.js
@@ -0,0 +1,89 @@
+var $canvas = $("#img-viewer");
+var canvas = $canvas.get(0);
+var context = canvas.getContext('2d');
+var img = null;
+
+var dx = $canvas.offset().left;
+var dy = $canvas.offset().top;
+
+function coordsInCanvas(x, y) {
+    return {x: x - dx, y: y - dy};
+}
+
+var MAX_WIDTH = 800;
+var MAX_HEIGHT = 800;
+
+function newSize(width, height) {
+    var ratio;
+    if (width > height) {
+        ratio = MAX_WIDTH / width;
+    } else {
+        ratio = MAX_HEIGHT / height;
+    }
+    return {width: width * ratio, height: height * ratio};
+}
+
+function showImage() {
+    img = new Image();
+    img.onload = function () {
+        displaySize = newSize(img.width, img.height);
+        canvas.height = displaySize.height;
+        canvas.width = displaySize.width;
+        context.drawImage(img, 0, 0, displaySize.width, displaySize.height);
+    }
+    img.src = $canvas.data("img-src");
+}
+
+var mouseIsDown = false;
+var drawedRect = false;
+var startX = null;
+var startY = null;
+
+var annotations = [];
+
+$canvas.on("mousedown", function (e) {
+    mouseIsDown = true;
+    var coords = coordsInCanvas(e.pageX, e.pageY);
+    startX = coords.x;
+    startY = coords.y;
+});
+
+$canvas.on("mouseup", function (e) {
+    mouseIsDown = false;
+    if (drawedRect) {
+        var coords = coordsInCanvas(e.pageX, e.pageY);
+        var rect = {
+            x: startX,
+            y: startY,
+            width: coords.x - startX,
+            height: coords.y - startY,
+        };
+        annotations.push(rect);
+    }
+})
+
+function redraw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(img, 0, 0, displaySize.width, displaySize.height);
+
+    for (var i = 0; i < annotations.length; i++) {
+        var r = annotations[i];
+        context.strokeStyle = "red";
+        context.strokeRect(r.x, r.y, r.width, r.height);
+    }
+}
+
+$canvas.on("mousemove", function(e) {
+    if (!mouseIsDown) {
+        return;
+    }
+    drawedRect = true;
+
+    redraw();
+
+    var coords = coordsInCanvas(e.pageX, e.pageY);
+    context.strokeStyle = "red";
+    context.strokeRect(startX, startY, coords.x - startX, coords.y - startY);
+})
+
+showImage();
