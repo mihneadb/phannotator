8605033e9319affcc48706cedb837b74d9f9323f
Andrei Petre <p31andrei@gmail.com>
2013-12-16 00:43:11 +0200

    Add models to admin.

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from server.models import *
+
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Annotation)
+admin.site.register(Comment)
